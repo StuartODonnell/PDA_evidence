b4522eb4504462b25c6273d5b6e79d48f03727cc
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-22 19:25:28 +0100

    adds passing integration test formultiplication function

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,11 +16,26 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+it ('should be able to add multiply 3x5 and get 15', function(){
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number3')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number5')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('15')
 
+})
 
 
 });
 
+// multiply 3x5 and get 15
+// divide 21/7 and get 3
+// add 1+4 and get 5
+// subtract 7-4 and get 3
+// concatenate multiple number button clicks
+// chain multiple operations together
+// clear the running total without affecting the calculation
 // calculator.add()
 // calculator.subtract()
 // calculator.multiply()
