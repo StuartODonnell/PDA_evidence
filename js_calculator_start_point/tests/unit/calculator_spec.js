1a362396886157354403dcc3d216fb89bfd73e48
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-22 19:14:58 +0100

    adds unit passing test for clearing calculator without affecting calculation

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -12,55 +12,61 @@ describe('calculator', function () {
   })
 
   it('should be able to add to running total',function(){
-  calculator.previousTotal = 1;
-  calculator.add(4);
-  assert.strictEqual(calculator.runningTotal, 5);
-})
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.strictEqual(calculator.runningTotal, 5);
+  })
 
-it('should be able to subtract from running total',function(){
-calculator.previousTotal = 7;
-calculator.subtract(4);
-assert.strictEqual(calculator.runningTotal, 3);
-})
+  it('should be able to subtract from running total',function(){
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    assert.strictEqual(calculator.runningTotal, 3);
+  })
 
-it('should be able to multiply running total',function(){
-calculator.previousTotal = 3;
-calculator.multiply(5);
-assert.strictEqual(calculator.runningTotal, 15);
-})
+  it('should be able to multiply running total',function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.strictEqual(calculator.runningTotal, 15);
+  })
 
-it('should be able to divide running total',function(){
-calculator.previousTotal = 21;
-calculator.divide(7);
-assert.strictEqual(calculator.runningTotal, 3);
-})
+  it('should be able to divide running total',function(){
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    assert.strictEqual(calculator.runningTotal, 3);
+  })
 
-it('concatenate multiple number button clicks', function(){
-  calculator.previousTotal = 0;
-  calculator.numberClick(1);
-  calculator.numberClick(1);
-  calculator.numberClick(1);
-  assert.strictEqual(calculator.runningTotal, 111);
-})
+  it('concatenate multiple number button clicks', function(){
+    calculator.previousTotal = 0;
+    calculator.numberClick(1);
+    calculator.numberClick(1);
+    calculator.numberClick(1);
+    assert.strictEqual(calculator.runningTotal, 111);
+  })
 
-it('should be able to divide running total',function(){
-calculator.numberClick(6);
-calculator.operatorClick("/");
-calculator.numberClick(3);
-calculator.operatorClick("*");
-calculator.numberClick(10);
-calculator.operatorClick("+");
-calculator.numberClick(5);
-calculator.operatorClick("-");
-calculator.numberClick(1);
-calculator.operatorClick("=");
-// calculator.multiply(3);
-// calculator.add(10);
-// calculator.subtract(3);
+  it('should be able to divide running total',function(){
+    calculator.numberClick(6);
+    calculator.operatorClick("/");
+    calculator.numberClick(3);
+    calculator.operatorClick("*");
+    calculator.numberClick(10);
+    calculator.operatorClick("+");
+    calculator.numberClick(5);
+    calculator.operatorClick("-");
+    calculator.numberClick(1);
+    calculator.operatorClick("=");
 
+    assert.strictEqual(calculator.runningTotal, 24);
+  })
 
-assert.strictEqual(calculator.runningTotal, 24);
-})
+  it('should clear the running total without affecting the calculation', function(){
+    calculator.numberClick(6);
+    calculator.operatorClick("/");
+    calculator.numberClick(3);
+    calculator.clearClick();
+    calculator.numberClick(2);
+    calculator.operatorClick("=");
+    assert.strictEqual(calculator.runningTotal, 3);
+  })
 
 
 });
