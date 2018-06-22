2f794d48dfef1fae4053e7ea8124b2892f135289
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-22 17:31:02 +0100

    adds passing unit test for divide function

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -13,26 +13,34 @@ describe('calculator', function () {
 
   it('should be able to add to running total',function(){
   calculator.previousTotal = 1;
-  calculator.add(5);
-  assert.strictEqual(calculator.runningTotal, 6);
+  calculator.add(4);
+  assert.strictEqual(calculator.runningTotal, 5);
 })
 
 it('should be able to subtract from running total',function(){
-calculator.previousTotal = 5;
+calculator.previousTotal = 7;
 calculator.subtract(4);
-assert.strictEqual(calculator.runningTotal, 1);
+assert.strictEqual(calculator.runningTotal, 3);
 })
 
 it('should be able to multiply running total',function(){
-calculator.previousTotal = 5;
-calculator.multiply(4);
-assert.strictEqual(calculator.runningTotal, 20);
+calculator.previousTotal = 3;
+calculator.multiply(5);
+assert.strictEqual(calculator.runningTotal, 15);
 })
 
+it('should be able to divide running total',function(){
+calculator.previousTotal = 21;
+calculator.divide(7);
+assert.strictEqual(calculator.runningTotal, 3);
+})
 
 });
 
-
+// multiply 3x5 and get 15
+// divide 21/7 and get 3
+// add 1+4 and get 5
+// subtract 7-4 and get 3
 // calculator.multiply()
 // calculator.divide()
 // calculator.numberClick()
